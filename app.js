0c3b6d6a114aa0cd03cca3c72c47e84675f95f23
Trivich Wongsammacheep <T876072@tac.co.th>
2016-12-11 12:44:50 +0700

    add env in start print

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require("express"),
     request = require("request");
     app = express();
-    
+
 app.use(function (req, res, next) {
     var getfrom = 'http://' + process.env.REDIRECTHOST + req.originalUrl;
     var followRedirectOption = !process.env.DO_NOT_REDIRECT_INTERNALLY;
     req.pipe(request({uri:getfrom,followRedirect:followRedirectOption})).pipe(res);
 });
-    
-    
+
+
 if (!module.parent) {
-    console.info('Listening ',process.env.PORT || 5000);
+    console.info(process.env.NODE_ENV +':Listening ',process.env.PORT || 5000);
     app.listen(process.env.PORT || 5000);
 }
